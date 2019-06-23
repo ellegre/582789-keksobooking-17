44cd5784c7e30d61399dd43e7953df35b5e8f8a8
elle <liubou.grebennikova@gmail.com>
2019-06-23 10:21:03 +0300

    Не работает up

--- js/main.js
+++ js/main.js
@@ -128,9 +128,8 @@ var leftPos = mapPinMain.offsetLeft + OFFSET_WIDTH / 2;
 
 address.value = leftPos + ', ' + topPos;
 
-mapPinMain.addEventListener('mouseup', function () {
-  activateMap();
-});
+
+
 
 // Валидация формы
 
@@ -155,4 +154,52 @@ checkIn.addEventListener('change', function () {
 });
 checkOut.addEventListener('change', function () {
   checkIn.value = checkOut.value;
+});
+
+
+// Перемещение маркера
+
+var mapPinMain = document.querySelector('.map__pin--main');
+mapPinMain.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    }
+
+  mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+  mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+
+  }
+
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault ();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+  activateMap();
+});
+
+
+mapPinMain.addEventListener('mouseup', function () {
+  activateMap();
 });
