12a4e05c9c7ed50e69c93454dbe0bc134f3febca
elle <liubou.grebennikova@gmail.com>
2019-07-01 22:15:19 +0300

    Создана функция load

--- js/pin.js
+++ js/pin.js
@@ -1,12 +1,12 @@
 'use strict';
 
 (function () {
-  var PROPERTY_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+  //var PROPERTY_TYPES = ['palace', 'flat', 'house', 'bungalo'];
   var ADS_NUMBER = 8;
   var PIN_HEIGHT = 70;
   var PIN_WIDTH = 50;
 
-  var createAvatars = function () {
+  /*var createAvatars = function () {
     var avatars = [];
     for (var i = 1; i < ADS_NUMBER + 1; i++) {
       if (i < 10) {
@@ -42,7 +42,8 @@
   };
 
   var propertyList = getPropertyList(ADS_NUMBER);
-var createMarker = function (marker) {
+*/
+  var createMarker = function (marker) {
     var markerElement = markerTemplate.cloneNode(true);
     markerElement.style.left = marker.location.x - PIN_WIDTH / 2 + 'px';
     markerElement.style.top = marker.location.y - PIN_HEIGHT + 'px';
@@ -56,59 +57,28 @@ var createMarker = function (marker) {
     .content
     .querySelector('button');
 
-  window.insert = function () {
-    var fragment = document.createDocumentFragment();
-    for (var j = 0; j < propertyList.length; j++) {
-      fragment.appendChild(createMarker(propertyList[j]));
-    }
-    return fragment;
+
+    window.insert = function () {
+      window.load(function (objects) {
+      var fragment = document.createDocumentFragment();
+         console.log(objects);
+      for (var j = 0; j < 5; j++) {
+        fragment.appendChild(createMarker(objects[j]));
+      }
+      return fragment;
+
+    });
   };
 
- window.load(function (markers) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < propertyList.length; i++) {
-      fragment.appendChild(createMarker(propertyList[j]));
-    }
-    similarListElement.appendChild(fragment);
-
-    userDialog.querySelector('.setup-similar').classList.remove('hidden');
-  });
-
-  var form = document.querySelector('.ad-form');
+  var notice = document.querySelector('.notice');
+  var form = notice.querySelector('.ad-form');
   form.addEventListener('submit', function (evt) {
     window.upload(new FormData(form), function (response) {
-      form.classList.add('hidden');
+      notice.classList.add('hidden');
     });
     evt.preventDefault();
   });
 
 
-/*
-    var successHandler = function (wizards) {
-    var fragment = document.createDocumentFragment();
-
-    for (var i = 0; i < 4; i++) {
-      fragment.appendChild(renderWizard(wizards[i]));
-    }
-    similarListElement.appendChild(fragment);
-
-    userDialog.querySelector('.setup-similar').classList.remove('hidden');
-  };
-
-  var errorHandler = function (errorMessage) {
-    var node = document.createElement('div');
-    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
-    node.style.position = 'absolute';
-    node.style.left = 0;
-    node.style.right = 0;
-    node.style.fontSize = '30px';
-
-    node.textContent = errorMessage;
-    document.body.insertAdjacentElement('afterbegin', node);
-  };
-
-  window.load(successHandler, errorHandler);
-  */
 
 }());
