d95763ecec2197946591252536ea1c33f2e579b7
elle <liubou.grebennikova@gmail.com>
2019-07-03 12:52:30 +0300

    Any не меняется

--- js/filter.js
+++ js/filter.js
@@ -3,10 +3,9 @@
 (function () {
 
   var housingType = document.querySelector('#housing-type');
-  var housingTypes = [];
   housingType.addEventListener('change', function () {
 
-    housingTypes = window.dataArray.filter(function(it) {
+    var housingTypes = window.dataArray.filter(function(it) {
       return it.offer.type === housingType.value;
 
     });
