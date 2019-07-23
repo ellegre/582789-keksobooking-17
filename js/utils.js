863c03fba9cbe1b64ade340652c3b75f028e2fd5
elle <liubou.grebennikova@gmail.com>
2019-07-23 20:49:42 +0300

    Замечения после первой проверки исправлены, кроме загрузки аватара

--- js/utils.js
+++ js/utils.js
@@ -12,12 +12,10 @@
       rand = Math.floor(rand);
       return rand;
     },
-
     getPropertyType: function (arr) {
       var arrRandNumber = Math.floor(Math.random() * arr.length);
       return arr[arrRandNumber];
     },
-
     clearPins: function () {
       var pins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
       pins = Array.from(pins);
